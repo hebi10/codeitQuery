15d7e3123ba4981d1ae689549b958ba1e2c85613
doyoung park <asdlkj0104@gmail.com>
2025-01-20 16:28:02 +0900

    250120 타입스크립트 적용 오류

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,12 +1,70 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { getPosts } from './api';
+import { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { getPosts, uploadPost } from './api';
 
-const HomePage: React.FC = () => {
-  const result = useQuery({ queryKey: ['posts'], queryFn: getPosts });
-  console.log(result);
+interface NewPostType {
+  username: string, 
+  content: string,
+}
 
-  return <div>홈페이지</div>;
+const HomePage:FC = () => {
+  const [content, setContent] = useState('');
+  const {
+    data: postsData,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts,
+    retry: 0,
+  });
+
+  const uploadPostMutation = useMutation({
+    mutationFn: (newPost) => uploadPost(newPost),
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newPost: NewPostType = { username: 'codeit', content };
+    uploadPostMutation.mutate(newPost);
+    setContent('');
+  };
+
+  if (isPending) return '로딩 중입니다...';
+
+  if (isError) return '에러가 발생했습니다.';
+
+  const posts = postsData?.results ?? [];
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            name="content"
+            value={content}
+            onChange={handleInputChange}
+          />
+          <button disabled={!content} type="submit">
+            업로드
+          </button>
+        </form>
+      </div>
+      <div>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              {post.user.name}: {post.content}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
--- src/api.ts
+++ src/api.ts
@@ -18,16 +18,38 @@ interface Post {
   user: User;
 }
 
-interface PostData {
+export interface PostData {
   data: {
     count: number;
     currentPage: number;
     hasMore: boolean;
     results: Post[];
-  };
+  },
+  results: Post[],
+}
+
+export async function getPostsByUsername(username: string) {
+  const response = await fetch(`${BASE_URL}/posts?username=${username}`);
+  return await response.json();
 }
 
 export async function getPosts(): Promise<PostData> {
   const response = await fetch(`${BASE_URL}/posts`);
+  return await response.json();
+}
+
+export async function uploadPost(newPost) {
+  const response = await fetch(`${BASE_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPost),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to upload the post.');
+  }
+
   return await response.json();
 }
